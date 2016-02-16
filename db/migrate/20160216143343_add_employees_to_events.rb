c957c5d0815f7a830f7d14eba0c4df22fbcaa9c8
Igor Chornous <ichornous@heliostech.fr>
2016-02-16 16:47:20 +0200

    Added model Employee and corresponding relations to Event

---
+++ db/migrate/20160216143343_add_employees_to_events.rb
@@ -0,0 +1,8 @@
+class AddEmployeesToEvents < ActiveRecord::Migration
+  def change
+    create_table :employees_events, id: false do |t|
+      t.belongs_to :event, index: true
+      t.belongs_to :employee, index: true
+    end
+  end
+end

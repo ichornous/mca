3a62477520668b502db38799cb652b0636a13cc5
Igor Chornous <igor@chornous.com>
2016-02-18 23:01:49 +0200

    Added support for windows build. Isolated unicorn

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
 
-  resources :visits do
-    resources :contacts
-  end
-
   #get 'visit/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'visits#index'
+  root 'events#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

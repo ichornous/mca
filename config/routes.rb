03aa758aa9182916f50ee9cb5e0d9f430b9ba469
Igor Chornous <ichornous@heliostech.fr>
2016-03-17 16:38:24 +0200

    Scaffolded service controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :services
   resources :events
   resources :workshops
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160317144253) do
-
   create_table "employees", force: :cascade do |t|
     t.string   "first_name"
     t.string   "second_name"

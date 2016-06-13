cbbf253c4606f56ad7f27e4f89cc1c64591c844b
Igor Chornous <ichornous@heliostech.fr>
2016-06-13 13:37:25 +0300

    Merged Booking and Order in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,13 +27,75 @@ cars = Car.create([
                   ])
 
 orders = Order.create([
-                          { client: clients[0], car: cars[1], state: 'NEW', workshop: workshops[0] },  # 0
-                          { client: clients[0], car: cars[2], state: 'WORK', workshop: workshops[0] }, # 1
-                          { client: clients[1], car: cars[0], state: 'DONE', workshop: workshops[0] }, # 2
-                          { client: clients[1], car: cars[3], state: 'PAYED', workshop: workshops[0] },# 3
-                          { client: clients[1], car: cars[3], state: 'PAYED', workshop: workshops[0] },# 4
-                          { client: clients[2], car: cars[4], state: 'PAYED', workshop: workshops[0] },# 5
-                          { client: clients[3], car: cars[5], state: 'WORK', workshop: workshops[0] }, # 6
+                          {
+                              client: clients[0],
+                              car: cars[1],
+                              state: 'NEW',
+                              workshop: workshops[0],
+                              description: 'Just a poor guy',
+                              start_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
+                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2')
+                          },  # 0
+
+                          {
+                              client: clients[0],
+                              car: cars[2],
+                              state: 'WORK',
+                              workshop: workshops[0],
+                              description: 'Right from saloon',
+                              start_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
+                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2')
+                          }, # 1
+
+                          {
+                              client: clients[1],
+                              car: cars[0],
+                              state: 'DONE',
+                              workshop: workshops[0],
+                              description: 'Not his car',
+                              start_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
+                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2')
+                          }, # 2
+
+                          {
+                              client: clients[1],
+                              car: cars[3],
+                              state: 'PAYED',
+                              workshop: workshops[0],
+                              description: 'Have already been here',
+                              start_date: DateTime.new(2016, 4, 11, 12, 0, 0, '+2'),
+                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2')
+                          },# 3
+
+                          {
+                              client: clients[1],
+                              car: cars[3],
+                              state: 'PAYED',
+                              workshop: workshops[0],
+                              description: 'Is he a cat??',
+                              start_date: DateTime.new(2016, 4, 11, 12, 0, 0, '+2'),
+                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2')
+                          },# 4
+
+                          {
+                              client: clients[2],
+                              car: cars[4],
+                              state: 'PAYED',
+                              workshop: workshops[0],
+                              description: 'Not his car',
+                              start_date: DateTime.new(2016, 4, 11, 12, 0, 0, '+2'),
+                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2')
+                          },# 5
+
+                          {
+                              client: clients[3],
+                              car: cars[5],
+                              state: 'WORK',
+                              workshop: workshops[0],
+                              description: 'Not his car',
+                              start_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
+                              end_date: DateTime.new(2016, 4, 13, 12, 0, 0, '+2')
+                          }, # 6
                       ])
 
 order_services = OrderService.create([
@@ -49,49 +111,6 @@ order_services = OrderService.create([
                                          {order: orders[5], service: services[1] },
                                          {order: orders[6], service: services[2] },
                                      ])
-visits = Booking.create([
-                          {
-                              description: 'Just a poor guy',
-                              start_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              order: orders[0]
-                          },
-                          {
-                              description: 'Right from saloon',
-                              start_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              order: orders[1]
-                          },
-                          {
-                              description: 'Not his car',
-                              start_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              order: orders[2]
-                          },
-                          {
-                              description: 'Have already been here',
-                              start_date: DateTime.new(2016, 4, 11, 12, 0, 0, '+2'),
-                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              order: orders[3]
-                          },
-                          {
-                              description: 'Is he a cat??',
-                              start_date: DateTime.new(2016, 4, 11, 12, 0, 0, '+2'),
-                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              order: orders[4]
-                          },
-                          {
-                              description: 'Not his car',
-                              start_date: DateTime.new(2016, 4, 11, 12, 0, 0, '+2'),
-                              end_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              order: orders[5]
-                          },
-                          {
-                              description: 'Not his car',
-                              start_date: DateTime.new(2016, 4, 12, 12, 0, 0, '+2'),
-                              end_date: DateTime.new(2016, 4, 13, 12, 0, 0, '+2'),
-                              order: orders[6]
-                          },
-                      ])
+
 
 

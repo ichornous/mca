af18811fd352b4627d0e6f6c7f7193bcc634197a
Igor Chornous <ichornous@heliostech.fr>
2016-06-21 11:37:40 +0300

    Enhanced locales for orders#new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,7 @@
 //= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require bootstrap-datepicker
 //= require bootstrap-datepicker/js/locales/bootstrap-datepicker.ru
+//= require typeahead.js/dist/typeahead.bundle.min.js
 //= require fullcalendar/dist/fullcalendar
 //= require fullcalendar/dist/lang/ru.js
 //= require datatables/media/js/jquery.dataTables.js
